dd529fecd7286c0bf8587f3cd2a2bf4ccab952e6
Abhishek Yadav <yadavabhi1522@gmail.com>
2021-11-20 14:52:22 +0530

    Added theme

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/utils/drawer.dart';
+import 'package:flutter_catalog/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
   @override
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -24,22 +24,27 @@ class MyDrawer extends StatelessWidget {
                 Icons.home,
                 color: Colors.white,
               ),
-              title: Text("Home", textScaleFactor: 1.2, 
-              style: TextStyle(color: Colors.white),),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             ListTile(
               leading: Icon(
                 Icons.person,
                 color: Colors.white,
               ),
-              title: Text("Profile", textScaleFactor: 1.2, style: TextStyle(color: Colors.white)),
+              title: Text("Profile",
+                  textScaleFactor: 1.2, style: TextStyle(color: Colors.white)),
             ),
             ListTile(
               leading: Icon(
                 Icons.email,
                 color: Colors.white,
               ),
-              title: Text("Contact Us", textScaleFactor: 1.2, style: TextStyle(color: Colors.white)),
+              title: Text("Contact Us",
+                  textScaleFactor: 1.2, style: TextStyle(color: Colors.white)),
             ),
           ],
         ),
